3efd11e20349e2101b8a3635ef342e74f9c44857
Rodrigo L.C <rodrigolealc@ciencias.unam.mx>
2020-06-17 16:28:47 +0100

    Added functions to read trajectory data from csv

--- shqod/__init__.py
+++ shqod/__init__.py
@@ -1,6 +1,8 @@
 """shqod - Origin-Destination tools to work with the SHQ dataset."""
 
 from .io import (
+    read_trajec_csv,
+    trajecs_from_df,
     load_trajecs,
     load_trajecs_lex
 )
--- shqod/io.py
+++ shqod/io.py
@@ -1,9 +1,75 @@
 """Read and write data."""
 
-from typing import List, Iterable
+from typing import Union, Optional, Tuple, List, Iterable
 from .dtypes import Trajec, LexTrajec
 
 import json
+import pandas as pd
+
+LoadedTrajec = Union[Iterable[Trajec], Iterable[LexTrajec]]
+
+
+def read_trajec_csv(filename: str,
+                    return_length: bool = False) -> Tuple[pd.DataFrame, Optional[int]]:
+    """
+    Read a csv file containing trajectory data into a DataFrame.
+
+    Parameters
+    ----------
+    filename : str
+        The name of the csv file.
+    return_length : bool, optional
+        Return the number of entries loaded (default is False).
+
+    Returns
+    -------
+    Tuple[pd.DataFrame, Optional[int]]
+        The loaded datafram and optionally its length.
+
+    """
+    df = pd.read_csv(filename)
+    assert 'trajectory_data' in df, 'error: file does not contain trajectory data'
+    nb_entries = len(df)
+
+    return (df, nb_entries) if return_length else df
+
+
+def trajecs_from_df(df: pd.DataFrame,
+                    lexico: bool = False,
+                    grid_width: int = None) -> LoadedTrajec:
+    """
+    Parse JSON stored in a DataFrame and return a generator with trajectories.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The data loaded as a DataFrame.
+    lexico : bool, optional
+        Use lexicographic indexing.
+    grid_width : int or None, optional
+        Provide the width of the grid and use it to calculate the lexicographic
+        index (defualt is None).
+
+    Returns
+    -------
+    Union[Iterable[Trajec], Iterable[LexTrajec]]
+        Depending on the value set for the flag `lexico`, return a generator
+        with trajectories in the form of (x,y) coordinates or as a single index.
+
+    """
+    assert 'trajectory_data' in df,\
+        'error: DataFrame does not contain trajectory data'
+    if lexico:
+        assert grid_width is not None,\
+            'error: grid_width is needed for lexicographic trajectory'
+
+    for _, row in df.iterrows():
+        data = json.loads(row.trajectory_data)['player']
+
+        if lexico:
+            yield map(lambda el: el['x'] * grid_width + el['y'], data)
+        else:
+            yield map(lambda el: (el['x'], el['y']), data)
 
 
 def load_trajecs(files: Iterable[str]) -> List[Trajec]:
