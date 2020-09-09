b743f7175edcda715f05f739d5a5024f3e843e63
Rodrigo L.C <rodrigolealc@ciencias.unam.mx>
2020-09-09 19:29:17 +0100

    Updated script for p-values

--- scripts/calculate_pvalues.py
+++ scripts/calculate_pvalues.py
@@ -4,40 +4,46 @@ import scipy.stats as st
 from tabulate import tabulate
 
 
-def pvalues(df, correct_gameplay=False):
+def pvalues(df, groups, correct_gameplay=False):
+    """Calculate p-values for the given groups."""
 
+    g1, g2 = groups
     levels = set(df.level)
-    # n = len(filenames_one)
-    # assert len(filenames_two) == n  # by desing we have the same participants
+
+    # The test names come directly from generate_tidy_results.py
+    test_names = [
+        'Frob. norm', 'Inf. norm', 'Restrict. sum',
+        'Mobty functional', 'Fractal dim.', 'Tot. length'
+    ]
 
     if correct_gameplay:
         # We use the results for the first two levels to correct for gameplay
         assert 1 in levels and 2 in levels
 
-        one = df.loc[df.level == 1].drop(columns=['group', 'level'])
-        two = df.loc[df.level == 2].drop(columns=['group', 'level'])
+        one = df.loc[df.level == 1][test_names]
+        two = df.loc[df.level == 2][test_names]
         sum_df = one + two.values  # .values for elementwise ignoring indices
-        cols = df.drop(columns=['group', 'level']).columns
 
         correc_df = df.copy()
 
         for l in levels.difference((1, 2)):
             idx = correc_df.level == l
-            correc_df.loc[idx, cols] = df.loc[idx, cols] / sum_df.values
+            correc_df.loc[idx, test_names] = df.loc[idx, test_names] / sum_df.values
     else:
         correc_df = df
 
     # p-values
     level_pvals = []
-    for l in levels.difference((1, 2)):
-        a = correc_df.loc[(correc_df.level == l) & (correc_df.group == 'A')]
-        b = correc_df.loc[(correc_df.level == l) & (correc_df.group == 'B')]
+    for lvl in levels.difference((1, 2)):
+        lvl_idx = correc_df.level == lvl
+        first = correc_df.loc[lvl_idx & (correc_df.group == g1)]
+        second = correc_df.loc[lvl_idx & (correc_df.group == g2)]
 
-        pvals = [st.ttest_ind(a.values[:, i],
-                              b.values[:, i],
+        pvals = [st.ttest_ind(first[test_names].values[:, i],
+                              second[test_names].values[:, i],
                               equal_var=False).pvalue
                  for i in range(6)]
-        level_pvals.append([l] + pvals)
+        level_pvals.append([lvl] + pvals)
 
     return level_pvals
 
@@ -46,12 +52,13 @@ if __name__ == '__main__':
     # Parse the input arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('input_file', help='file containing tidy data')
-    parser.add_argument('-g', '--groups', default='AB',
+    parser.add_argument('-g', '--groups', nargs='+',
+                        default=['e3e4', 'e3e3'],
                         help='the label of two groups to compare')
     args = parser.parse_args()
 
     filename = args.input_file
-    groups = args.groups.upper()
+    groups = args.groups
     assert filename.endswith('.csv'), 'input should be csv file'
     assert len(groups) == 2, 'should give two groups'
 
@@ -61,8 +68,8 @@ if __name__ == '__main__':
     results_df = results_df.loc[idx]
 
     # Calculate the p-values with and without correction
-    pvals = pvalues(results_df, correct_gameplay=False)
-    correc_pvals = pvalues(results_df, correct_gameplay=True)
+    pvals = pvalues(results_df, groups, correct_gameplay=False)
+    correc_pvals = pvalues(results_df, groups, correct_gameplay=True)
 
     head = ['Level', 'Frob. norm', 'Inf. norm', 'Restrict. sum',
             'Mobty functional', 'Fractal dim.', 'Tot. length']
